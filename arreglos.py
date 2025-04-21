472569cbd189832819bc01090324659e6a8f2b70
Potasio230 <raulferrini230@gmail.com>
2025-04-21 13:29:55 -0400

    avances de la clase de arreglos

---
+++ arreglos.py
@@ -0,0 +1,15 @@
+#HOLa
+
+#esto es para guardar varias cosas en una variable
+frutas = ["manzana" , "pera" , "frutilla"]
+#se imprime cero para la manzana, 1 para pera, etc
+print(frutas [0])
+
+#los objetos 
+#se usan con {estas cositas}
+estudiante = [ { 'nombre' : "maxi", 'edad' : 20, 'curso' : "2A"}, 
+              { 'nombre' : "richar" , 'edad' : 21, 'curso' : "3A"}   ]
+
+#tenemos que especificar el valor en concreto que queremos que salga
+
+print(estudiante [0] ['curso'])
